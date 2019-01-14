448726109b2153ddeb4531b6709ea68b188138aa
YetiWilson <abaldie@gmail.com>
2019-01-14 14:27:03 -0800

    add boot strap script.

---
+++ boot_strap.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+set -e
+
+if [ $# -ne 2 ]; then
+  echo "Please provide your redhat username & password"
+  echo "Syntax is $0 <username> <password>"
+  exit 1
+fi
+
+echo "Registering the host"
+subscription-manager register --username $1 --password $2 --auto-attach
+
+echo "Enabling optional RPMs"  
+subscription-manager repos --enable=rhel-7-server-optional-rpms
+
+echo "Enabling Extras"   
+subscription-manager repos --enable=rhel-7-server-extras-rpms
+
+echo "Installing epel" 
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+ 
+echo "Installing pip"    
+yum install python-pip
+
+echo "Installing Ansible"      
+sudo pip install ansible
